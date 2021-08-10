ad6bd6c815d996eb9fadf12b58c441de441dffdc
Hiroshi Murayama <opiopan@gmail.com>
2021-08-10 22:58:34 +0900

    add windows meesage loop and hidden main window to console mockup apps

--- modules/build_lua.bat
+++ modules/build_lua.bat
@@ -1,10 +1,12 @@
-curl -s https://www.lua.org/ftp/lua-5.4.3.tar.gz | tar -zxvf -
-cd lua-5.4.3\src
+set VERSION=5.4.3
+
+curl -s https://www.lua.org/ftp/lua-%VERSION%.tar.gz | tar -zxvf -
+cd lua-%VERSION%\src
 cl /MD /O2 /c /DLUA_BUILD_AS_DLL *.c
 ren lua.obj lua.o
 ren luac.obj luac.o
-link /DLL /IMPLIB:lua5.4.3.lib /OUT:lua5.3.5.dll *.obj
-link /OUT:lua.exe lua.o lua5.4.3.lib
-lib /OUT:lua5.4.3-static.lib *.obj
-link /OUT:luac.exe luac.o lua5.4.3-static.lib
+link /DLL /IMPLIB:lua%VERSION%.lib /OUT:lua%VERSION%.dll *.obj
+link /OUT:lua.exe lua.o lua%VERSION%.lib
+lib /OUT:lua%VERSION%-static.lib *.obj
+link /OUT:luac.exe luac.o lua%VERSION%-static.lib
 cd ..\..
--- src/mockup/testmock.cpp
+++ src/mockup/testmock.cpp
@@ -5,6 +5,57 @@
 #include <windows.h>
 #include "mappercore.h"
 
+static LRESULT CALLBACK window_proc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    switch(uMsg) {
+    case WM_CREATE:
+        ::ShowWindow(hWnd, SW_HIDE);
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage( 0 );
+        break;
+    }
+    return ::DefWindowProc( hWnd, uMsg, wParam, lParam );
+}
+
+static HWND create_main_window(){
+    auto hInstance = ::GetModuleHandle(nullptr);
+
+    WNDCLASSEXA tWndClass;
+    tWndClass.cbSize        = sizeof(WNDCLASSEX);
+    tWndClass.style         = CS_HREDRAW | CS_VREDRAW;
+    tWndClass.lpfnWndProc   = window_proc;
+    tWndClass.cbClsExtra    = 0;
+    tWndClass.cbWndExtra    = 0;
+    tWndClass.hInstance     = hInstance; 
+    tWndClass.hIcon         = ::LoadIcon(nullptr, IDI_APPLICATION);
+    tWndClass.hCursor       = ::LoadCursor(nullptr, IDC_ARROW);
+    tWndClass.hbrBackground = reinterpret_cast<HBRUSH>( COLOR_WINDOW + 1 );
+    tWndClass.lpszMenuName  = nullptr;
+    tWndClass.lpszClassName = "testmock_main";
+    tWndClass.hIconSm       = nullptr;
+    if (::RegisterClassExA(&tWndClass) == 0) {
+        return nullptr;
+    }
+
+    auto hWnd = ::CreateWindowExA(
+          0                       // extended window style
+        , tWndClass.lpszClassName // pointer to registered class name
+        , "Test of mapper"        // pointer to window name
+        , WS_OVERLAPPEDWINDOW     // window style
+        , CW_USEDEFAULT           // horizontal position of window
+        , CW_USEDEFAULT           // vertical position of window
+        , 640                     // window width
+        , 480                     // window height
+        , nullptr                 // handle to parent or owner window
+        , nullptr                 // handle to menu, or child-window identifier
+        , hInstance               // handle to application instance
+        , nullptr                 // pointer to window-creation data
+    );
+
+    return hWnd;
+}
+
+
 static bool console_handler(MapperHandle mapper, MCONSOLE_MESSAGE_TYPE type, const char *msg, size_t len){
     static std::mutex mutex;
     std::lock_guard lock(mutex);
@@ -23,11 +74,23 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    MapperHandle mapper = mapper_init(event_handler, console_handler, nullptr);
-    mapper_setLogMode(mapper, MAPPER_LOG_EVENT);
-    //mapper_setLogMode(mapper, 0);
-    auto rc = mapper_run(mapper, argv[1]);
-    mapper_terminate(mapper);
+    auto hWnd = create_main_window();
 
-    return rc;
+    auto thread = std::thread([argv, hWnd](){
+        MapperHandle mapper = mapper_init(event_handler, console_handler, nullptr);
+        mapper_setLogMode(mapper, MAPPER_LOG_EVENT);
+        //mapper_setLogMode(mapper, 0);
+        auto rc = mapper_run(mapper, argv[1]);
+        mapper_terminate(mapper);
+        ::PostMessageA(hWnd, WM_CLOSE, 0, 0);
+    });
+
+    MSG msg;
+    while( 0 != ::GetMessageA(&msg, NULL, 0, 0)){
+        ::TranslateMessage(&msg);
+        ::DispatchMessageA(&msg);
+    }
+
+    thread.join();
+    return msg.wParam;
 }
