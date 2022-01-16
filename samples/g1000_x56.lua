36f98e835eaf20cf4e9fccab63713d9539d424ca
Hiroshi Murayama <opiopan@gmail.com>
2022-01-16 20:58:50 +0900

    rearange sample screpts

--- samples/g1000_x56.lua
+++ samples/g1000_x56.lua
@@ -1,3 +1,6 @@
+tmod = require("test_module")
+tmod.print_package_path()
+
 g1000_dev = mapper.device({
     name = "SimHID G1000",
     type = "simhid",
@@ -196,17 +199,167 @@ function toggle_view()
 end
 
 viewport:set_mappings({
-    {event=g1000.AUX1D.down, action=toggle_view},
-    {event=g1000.AUX1U.down, action=toggle_view},
+    {event=g1000.AUX2D.down, action=toggle_view},
 })
 viewport:add_mappings({
-    {event=g1000.AUX2D.down, action=toggle_view},
     {event=g1000.AUX2U.down, action=toggle_view},
 })
 
 mapper.start_viewports()
+
+x56throttle_dev = mapper.device({
+    name = "X56 Throttle",
+    type = "dinput",
+    identifier = {name = "Saitek Pro Flight X-56 Rhino Throttle"},
+    options = {denylist = {"z", "rx", "ry", "rz", "slider1", "slider2"}},
+    modifiers = {
+        {name = "button33", modtype = "button", modparam={follow_down = 200}},
+        {name = "button34", modtype = "button"},
+        {name = "button35", modtype = "button"},
+        {name = "button36", modtype = "button"},
+        {name = "button28", modtype = "button"},
+        {name = "button29", modtype = "button"},
+    },
+})
+x56throttle = x56throttle_dev.events
+
+vjoy = mapper.virtual_joystick(1)
+local throttle1 = vjoy:get_axis("rx")
+local throttle2 = vjoy:get_axis("ry")
+local airbrake_open = vjoy:get_button(1)
+local airbrake_close = vjoy:get_button(2)
+local ab1 = vjoy:get_button(3)
+local ab2 = vjoy:get_button(4)
+
+local marm_on = vjoy:get_button(5)
+local marm_off = vjoy:get_button(6)
+
+local eng1idle = vjoy:get_button(7)
+local eng1cut = vjoy:get_button(8)
+local eng2idle = vjoy:get_button(9)
+local eng2cut = vjoy:get_button(10)
+local eng1start = vjoy:get_button(11)
+local eng2start = vjoy:get_button(12)
+local canopy_open = vjoy:get_button(13)
+local canopy_close = vjoy:get_button(14)
+local start_aux1 = vjoy:get_button(15)
+local start_aux2 = vjoy:get_button(16)
+
+local gearup = vjoy:get_button(17)
+local geardown = vjoy:get_button(18)
+local flapup = vjoy:get_button(19)
+local flapdown = vjoy:get_button(20)
+local hookup = vjoy:get_button(21)
+local hookdown = vjoy:get_button(22)
+local llight_on = vjoy:get_button(23)
+local llight_off = vjoy:get_button(24)
+local flight_aux1 = vjoy:get_button(31)
+local flight_aux2 = vjoy:get_button(32)
+
+local aa_mode = vjoy:get_button(25)
+local ag_mode = vjoy:get_button(26)
+local arm_aux1 = vjoy:get_button(27)
+local arm_aux2 = vjoy:get_button(28)
+local arm_aux3 = vjoy:get_button(29)
+local arm_aux4 = vjoy:get_button(30)
+
+local joymap_noab = {
+    {event=x56throttle.x.change, action=filter.duplicator(
+        filter.lerp(throttle1:value_setter(),{
+            {-1023, -1023},
+            {-609, -1023},
+            {1023, 1023},
+        }),
+        filter.branch(
+            {condition="falled", value=-900, action=ab1:value_setter(true)},
+            {condition="exceeded", value=-800, action=ab1:value_setter(false)}
+        )
+    )},
+    {event=x56throttle.y.change, action=filter.duplicator(
+        filter.lerp(throttle2:value_setter(),{
+            {-1023, -1023},
+            {-619, -1023},
+            {1023, 1023},
+        }),
+        filter.branch(
+            {condition="falled", value=-900, action=ab2:value_setter(true)},
+            {condition="exceeded", value=-800, action=ab2:value_setter(false)}
+        )
+    )},
+    {event=x56throttle.button33.up, action=airbrake_open:value_setter(true)},
+    {event=x56throttle.button33.down, action=filter.duplicator(
+        airbrake_open:value_setter(false),
+        airbrake_close:value_setter(true)
+    )},
+    {event=x56throttle.button33.following_down, action=airbrake_close:value_setter(false)},
+}
+
+local joymap_full = {
+    {event=x56throttle.x.change, action=throttle1:value_setter()},
+    {event=x56throttle.y.change, action=throttle2:value_setter()},
+    {event=x56throttle.button33.up, action=airbrake_open:value_setter(true)},
+    {event=x56throttle.button33.down, action=filter.duplicator(
+        airbrake_open:value_setter(false), airbrake_close:value_setter(true)
+    )},
+    {event=x56throttle.button33.following_down, action=airbrake_close:value_setter(false)},
+}
+
+local joymap_preflight = {
+    {event=x56throttle.button6.change, action=eng1idle:value_setter()},
+    {event=x56throttle.button7.change, action=eng1cut:value_setter()},
+    {event=x56throttle.button8.change, action=eng2idle:value_setter()},
+    {event=x56throttle.button9.change, action=eng2cut:value_setter()},
+    {event=x56throttle.button10.change, action=eng1start:value_setter()},
+    {event=x56throttle.button11.change, action=eng2start:value_setter()},
+    {event=x56throttle.button18.change, action=canopy_open:value_setter()},
+    {event=x56throttle.button19.change, action=canopy_close:value_setter()},
+    {event=x56throttle.button12.change, action=start_aux1:value_setter()},
+    {event=x56throttle.button13.change, action=start_aux2:value_setter()},
+}
+
+local joymap_inflight = {
+    {event=x56throttle.button6.change, action=gearup:value_setter()},
+    {event=x56throttle.button7.change, action=geardown:value_setter()},
+    {event=x56throttle.button8.change, action=flapup:value_setter()},
+    {event=x56throttle.button9.change, action=flapdown:value_setter()},
+    {event=x56throttle.button10.change, action=hookup:value_setter()},
+    {event=x56throttle.button11.change, action=hookdown:value_setter()},
+    {event=x56throttle.button12.change, action=llight_on:value_setter()},
+    {event=x56throttle.button13.change, action=llight_off:value_setter()},
+    {event=x56throttle.button18.change, action=flight_aux1:value_setter()},
+    {event=x56throttle.button19.change, action=flight_aux2:value_setter()},
+}
+
+local joymap_combat = {
+    {event=x56throttle.button6.change, action=aa_mode:value_setter()},
+    {event=x56throttle.button7.change, action=ag_mode:value_setter()},
+    {event=x56throttle.button8.change, action=arm_aux1:value_setter()},
+    {event=x56throttle.button9.change, action=arm_aux2:value_setter()},
+    {event=x56throttle.button10.change, action=arm_aux3:value_setter()},
+    {event=x56throttle.button11.change, action=arm_aux4:value_setter()},
+}
+
+local joymap ={
+    base = joymap_noab,
+    modal = {}
+}
+
+function update_secondary_mappings()
+    mapper.set_secondary_mappings(joymap.base)
+    mapper.add_secondary_mappings(joymap.modal)
+end
+
+update_secondary_mappings()
+
 mapper.set_primery_mappings({
     {event=mapper.events.change_aircraft, action=function (event, value)
+        if value.aircraft == "Airbus A320 Neo FlyByWire" then
+            joymap.base = joymap_full
+            update_secondary_mappings()
+        else
+            joymap.base = joymap_noab
+            update_secondary_mappings()
+        end
         if value.host then
             if value.aircraft then
                 mapper.print("    [sim]: "..value.host.." [aircraft]: "..value.aircraft) 
@@ -217,4 +370,30 @@ mapper.set_primery_mappings({
             mapper.print("    [sim]: disconnected")
         end
     end},
+
+    {event=x56throttle.button34.down, action=filter.duplicator(
+        function ()
+            joymap.modal = joymap_combat
+            update_secondary_mappings()
+            marm_on:set_value(true)
+        end,
+        filter.delay(200, marm_on:value_setter(false)))
+    },
+
+    {event=x56throttle.button35.down, action=function ()
+        joymap.modal = joymap_inflight
+        update_secondary_mappings()
+        marm_off:set_value(true)
+        mapper.delay(200, function () marm_off:set_value(false) end)
+    end},
+
+    {event=x56throttle.button36.down, action=function ()
+        joymap.modal = joymap_preflight
+        update_secondary_mappings()
+    end},
+
+    {event=x56throttle.button28.down, action=toggle_view},
+    {event=x56throttle.button29.down, action=toggle_view},
+    {event=g1000.AUX1U.down, action=function () mapper.reset_viewports() end},
+    {event=g1000.AUX1D.down, action=function () mapper.abort() end},
 })
