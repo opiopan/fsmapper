d66f3d6cc6ecd8d9ab14fa2f0fd758c8bce4b26d
Hiroshi Murayama <opiopan@gmail.com>
2024-08-28 21:37:53 +0900

    prepared to build c-module for Lua bundled within DCS world

--- modules/prepare_modules.bat
+++ modules/prepare_modules.bat
@@ -1,13 +1,18 @@
 @powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
 
 $lua_version = "5.4.6"
+$dcs_lua_version = "5.1.5"
 $vjoysdk = "vJoy218SDK-291116.zip"
 $vjoysdk_dest = "vJoySDK"
 $vjoysdk_header = "vjoyinterface.h"
 $vjoysdk_header_path = $vjoysdk_dest + "\SDK\inc\" + $vjoysdk_header
+
 $lua_dir = "lua-" + $lua_version
 if (Test-Path $lua_dir){
     Remove-Item $lua_dir -Recurse
+}
+if (Test-Path lua-5.4){
+    Remove-Item lua-5.4 -Recurse
 }
 $download_cmd = "curl.exe https://www.lua.org/ftp/" + $lua_dir + ".tar.gz | tar.exe -zxvf -"
 cmd /c $download_cmd
@@ -16,12 +21,35 @@ if (Test-Path $lua_dir){
     cl /MT /O2 /c /DLUA_BUILD_AS_DLL *.c
     Rename-Item -Path lua.obj lua.o
     Rename-Item -Path luac.obj luac.o
-    link /DLL /IMPLIB:lua$lua_version.lib /OUT:lua$lua_version.dll *.obj
-    link /OUT:lua.exe lua.o lua$lua_version.lib
-    lib /OUT:lua$lua_version-static.lib *.obj
-    link /OUT:luac.exe luac.o lua$lua_version-static.lib
+    link /DLL /IMPLIB:lua5.4.lib /OUT:lua5.4.dll *.obj
+    link /OUT:lua.exe lua.o lua5.4.lib
+    lib /OUT:lua5.4-static.lib *.obj
+    link /OUT:luac.exe luac.o lua5.4-static.lib
     Set-Location ..\..
 }
+Rename-Item $lua_dir lua-5.4
+
+$dcs_lua_dir = "lua-" + $dcs_lua_version
+if (Test-Path $dcs_lua_dir){
+    Remove-Item $dcs_lua_dir -Recurse
+}
+if (Test-Path lua-5.1){
+    Remove-Item lua-5.1 -Recurse
+}
+$download_cmd = "curl.exe https://www.lua.org/ftp/" + $dcs_lua_dir + ".tar.gz | tar.exe -zxvf -"
+cmd /c $download_cmd
+if (Test-Path $dcs_lua_dir){
+    Set-Location lua-$dcs_lua_version\src
+    cl /MT /O2 /c /DLUA_BUILD_AS_DLL *.c
+    Rename-Item -Path lua.obj lua.o
+    Rename-Item -Path luac.obj luac.o
+    link /DLL /IMPLIB:lua5.1.lib /OUT:lua5.1.dll *.obj
+    link /OUT:lua.exe lua.o lua5.1.lib
+    lib /OUT:lua5.1-static.lib *.obj
+    link /OUT:luac.exe luac.o lua5.1-static.lib
+    Set-Location ..\..
+}
+Rename-Item $dcs_lua_dir lua-5.1
 
 if (!(Test-Path $vjoysdk_dest)){
     New-Item $vjoysdk_dest -ItemType Directory
