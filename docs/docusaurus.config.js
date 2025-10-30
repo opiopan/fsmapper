f7ec089cc6ed8bd3e00dd41e28ed8f4380b6b515
Hiroshi Murayama <opiopan@gmail.com>
2025-10-30 21:57:36 +0900

    upgraded Docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -25,7 +25,12 @@ const config = {
   projectName: 'fsmapper', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
